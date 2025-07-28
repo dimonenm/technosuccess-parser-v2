552d09cfc874cd7ba4d883b29bcb6fda4aa48137
exp <arzuakov@yahoo.com>
2025-07-29 00:58:33 +0300

    29.07.2025 fix prizma 2

--- server/src/main.ts
+++ server/src/main.ts
@@ -5,14 +5,14 @@ import * as cookieParser from 'cookie-parser'
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-// app.use(cookieParser)
+app.use(cookieParser())
 
-// app.enableCors({
-//   origin: ['http://localhost:3001'],
-//   credentials: true,
-//   exposedHeaders: 'set-cookie'
+app.enableCors({
+  origin: ['http://localhost:3001'],
+  credentials: true,
+  exposedHeaders: 'set-cookie'
 
-// })
+})
 
   await app.listen(process.env.PORT ?? 3000);
 }
--- server/src/user/user.controller.ts
+++ server/src/user/user.controller.ts
@@ -1,13 +1,17 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { UserService } from './user.service'
 import { CreateUserDto } from './dto/createUser.dto'
 
 @Controller('user')
 export class UserController {
-	constructor (private userService: UserService){}
+  constructor(private userService: UserService) {}
 
-	@Post('create')
-	createUser(@Body() createUserDto: CreateUserDto){
-		return this.userService.createUser(createUserDto)
-	}
+  @Post('create')
+  createUser(@Body() createUserDto: CreateUserDto) {
+    return this.userService.createUser(createUserDto);
+  }
+  @Get('get-all')
+  findAllUsers() {
+    return this.userService.findAllUsers();
+  }
 }
