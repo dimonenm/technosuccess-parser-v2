4bbde35fcd263c3410a8fbcc3da6105a6fa6a1c1
arzuakov@yahoo.com <arzuakov@yahoo.com>
2025-07-29 17:52:11 +0300

    29.07.2025

--- server/src/user/user.controller.ts
+++ server/src/user/user.controller.ts
@@ -1,17 +1,21 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common'
 import { UserService } from './user.service'
 import { CreateUserDto } from './dto/createUser.dto'
 
 @Controller('user')
 export class UserController {
-  constructor(private userService: UserService) {}
+  constructor(private userService: UserService) { }
 
   @Post('create')
   createUser(@Body() createUserDto: CreateUserDto) {
-    return this.userService.createUser(createUserDto);
+    return this.userService.createUser(createUserDto)
   }
   @Get('get-all')
   findAllUsers() {
-    return this.userService.findAllUsers();
+    return this.userService.findAllUsers()
+  }
+  @Get('find-by-id')
+  findById(@Query() param: { id: string }) {
+    return param.id
   }
 }
--- server/src/user/user.service.ts
+++ server/src/user/user.service.ts
@@ -1,17 +1,53 @@
-import { Injectable } from '@nestjs/common'
+import { ConflictException, Injectable } from '@nestjs/common'
 import { User } from 'prisma/generated/prisma'
 import { CreateUserDto } from './dto/createUser.dto'
 import { PrismaService } from '../prisma.service'
+import { hash } from 'argon2'
 
 @Injectable()
 export class UserService {
 	constructor(private prisma: PrismaService) { }
 
 	async createUser(userDto: CreateUserDto): Promise<User | null> {
-		return this.prisma.user.create({data: userDto})
+
+		const isExist = await this.findByEmail(userDto.email)
+
+		if (isExist) {
+			throw new ConflictException('Регистрация не удалась. Пользователь с таким email уже существует.')
+		}
+
+		const newUser = {
+			email: userDto.email,
+			name: userDto.name ? userDto.name : '',
+			password: userDto.password ? await hash(userDto.password) : ''
+		}
+
+		return this.prisma.user.create(
+			{
+				data: newUser
+			}
+		)
 	}
 
 	async findAllUsers(): Promise<User[] | null> {
 		return this.prisma.user.findMany()
+	}
+
+	async findByEmail(email: string) {
+
+		const user = await this.prisma.user.findUnique({
+			where: { email }
+		})
+
+		return user
+	}
+
+	async findById(id: string) {
+
+		const user = await this.prisma.user.findUnique({
+			where: { id }
+		})
+
+		return user
 	}
 }
